f3a956f330175400a8be8433ac386e8dc3bec46b
iainbrux <me@iainbrux.com>
2021-06-30 21:47:49 +0100

    start simple node-ts calc function

---
+++ calculator.ts
@@ -0,0 +1,24 @@
+type Operation = 'multiply' | 'add' | 'divide';
+type Result = number;
+
+const calculator = (a: number, b: number, op: Operation): Result => {
+  switch (op) {
+    case 'multiply':
+      return a * b;
+    case 'divide':
+      if (b === 0) throw new Error('Can\'t divide by 0!');
+      return a / b;
+    case 'add':
+      return a + b;
+    default:
+      throw new Error('Operation is not multiply, add or divide!')
+  }
+}
+
+console.log(process.env)
+
+try {
+  console.log(calculator(1, 5, 'divide'))
+} catch (err) {
+  console.log('Something went wrong, error message: ', err.message)
+}
